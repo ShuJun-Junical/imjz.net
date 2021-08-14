a893613789e9fe48a0e5d87aabd0add718091f40
JupiterJun <hbbdmjz@163.com>
2021-08-14 10:30:18 +0800

    首次提交

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: '青章浚的小站', // Title for the site. This will be displayed in the navbar.
+    theme: '@vuepress/theme-blog',
+    themeConfig: {
+        // Please keep looking down to see the available options.
+    },
+    dest: './docs'
+}
---
+++ blog/_posts/2021-8-14-hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World
+date: 2021-08-14
+author: JupiterJun
+location: Baoding
+---
+
+> This is official blog theme.
+
+My content.
