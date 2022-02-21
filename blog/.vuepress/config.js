42a85894faa390ef2129c246f0d82635b7ca936e
青章浚 <hbbdmjz@163.com>
2022-02-21 18:45:52 +0800

    备案

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -23,12 +23,16 @@ module.exports = {
             ],
             copyright: [
                 {
+                    text: '琼ICP备2022001156号-1',
+                    link: 'https://beian.miit.gov.cn/'
+                },
+                {
                     text: '本站文章采用 CC-BY-NC-SA 4.0 国际许可协议进行许可。',
                     link: 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh'
                 },
                 {
                     text: '版权所有 © 2022 马浚哲'
-                }
+                },
             ],
         },
         frontmatters: [
