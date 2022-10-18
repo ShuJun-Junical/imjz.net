d613ace9120db17c99a9edcae9c443aa90d662a6
纾浚 <Junical@email.cn>
2022-10-18 16:53:19 +0800

    tmp

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -4,7 +4,7 @@ import NewLayout from './components/NewLayout.vue'
 import Archives from './components/Archives.vue'
 import Tags from './components/Tags.vue'
 import Page from './components/Page.vue'
-import Comment from './components/Comment.vue'
+// import Comment from './components/Comment.vue'
 
 import './custom.css'
 
